1f23125c006beae68e5b98ee51aa1509379ebb3d
thugborean <alexander.martensson@gmail.com>
2025-06-19 19:01:44 +0200

    AST: Refactored NodeType somewhat

--- src/main/java/io/github/thugborean/ast/node/types/NodeType.java
+++ src/main/java/io/github/thugborean/ast/node/types/NodeType.java
@@ -2,13 +2,10 @@ package io.github.thugborean.ast.node.types;
 
 import io.github.thugborean.ast.node.NodeAST;
 import io.github.thugborean.ast.visitor.ASTVisitor;
-
-public class NodeType implements NodeAST{
-
+public abstract class NodeType implements NodeAST{
     @Override
     public <T> T accept(ASTVisitor<T> visitor) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'accept'");
     }
-    
 }
