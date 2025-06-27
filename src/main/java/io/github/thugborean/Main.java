141113c11c9ebacba3242ddc76841b8c4507a966
thugborean <alexander.martensson@gmail.com>
2025-06-27 17:13:16 +0200

    MAIN: Some cleanup, args will return

--- src/main/java/io/github/thugborean/Main.java
+++ src/main/java/io/github/thugborean/Main.java
@@ -6,24 +6,7 @@ import io.github.thugborean.lexer.Lexer;
 import io.github.thugborean.repl.REPL;
 
 public class Main {
-    private static String source;
-    private static Scanner scanner;
-    private static Lexer lexer; // Maybe bad because double define WIP
     public static void main(String[] args ) {
-
-        // If given no arguments, run REPL
-        // if (args[0].isEmpty()) {
             REPL.run();
-    //     } else if(args[0] instanceof String) {
-    //     // Set the source to the input file
-    //         scanner = new Scanner(args[0]);
-    //         source = scanner.toString();
-
-    //         lexer = new Lexer();
-    //         lexer.tokenize(source);
-    //         lexer.toString();
-    //     }
-    // }
-
     }
 }
